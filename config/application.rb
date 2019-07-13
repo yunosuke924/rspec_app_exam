e9489c5d534684f82461ccaea87d01c89847ed81
yuji91 <yuji91.1227@gmail.com>
2019-07-13 18:02:03 +0900

    Modify: rails generate実行時にModelSpecとFactoryを作成させる （SystemSpecは自動生成されないので、別途手動でファイルを作成する）

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,15 @@ module Exam01:Bugfix
     config.generators do |g|
       g.assets false
       g.helper false
-      g.test_framework :false
+      g.test_framework :rspec,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       model_specs: true,
+                       request_specs: false,
+                       fixtures: true
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
     end
   end
 end
