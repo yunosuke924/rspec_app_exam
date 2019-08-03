a17fa59c1543c26ee7e63dfb24615d60759b9645
yuji91 <yuji91.1227@gmail.com>
2019-08-03 20:46:52 +0900

    Remove: 不要なコメントを削除

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,23 +1,18 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[show edit update destroy]
 
-  # GET /projects
   def index
     @projects = Project.all
   end
 
-  # GET /projects/1
   def show; end
 
-  # GET /projects/new
   def new
     @project = Project.new
   end
 
-  # GET /projects/1/edit
   def edit; end
 
-  # POST /projects
   def create
     @project = Project.new(project_params)
 
@@ -28,7 +23,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /projects/1
   def update
     if @project.update(project_params)
       redirect_to @project, notice: 'Project was successfully updated.'
@@ -37,7 +31,6 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # DELETE /projects/1
   def destroy
     @project.destroy
     redirect_to projects_url, notice: 'Project was successfully destroyed.'
@@ -45,12 +38,10 @@ class ProjectsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_project
     @project = Project.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def project_params
     params.require(:project).permit(:name, :status, :release_date)
   end
