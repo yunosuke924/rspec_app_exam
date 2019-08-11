b5fff756d60126aa8439fca5d3897ac64edad743
yuji91 <yuji91.1227@gmail.com>
2019-08-11 18:10:50 +0900

    Fix: Rubocop -a の実行と不要なコメントを削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,26 +1,19 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
   before_action :set_project
 
-  # GET /tasks
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1
-  def show
-  end
+  def show; end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /tasks
   def create
     @task = Task.new(task_params)
 
@@ -31,7 +24,6 @@ class TasksController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tasks/1
   def update
     if @task.update(task_params)
       redirect_to [@project, @task], notice: 'Task was successfully updated.'
@@ -40,24 +32,22 @@ class TasksController < ApplicationController
     end
   end
 
-  # DELETE /tasks/1
   def destroy
     @task.destroy
     redirect_to project_tasks_url, notice: 'Task was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    def set_project
-      @project = Project.find(params[:project_id])
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-    # Only allow a trusted parameter "white list" through.
-    def task_params
-      params.require(:task).permit(:title, :status, :deadline, :completion_date, :description).merge(project_id: params[:project_id])
-    end
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :status, :deadline, :completion_date, :description).merge(project_id: params[:project_id])
+  end
 end
