09b71a61a716f43d59b648eb3c3adfcb4da6c6a8
yuji91 <yuji91.1227@gmail.com>
2019-08-11 18:07:16 +0900

    Add: ModelにAssociationとenumを追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :status, presence: true
+  has_many :tasks, dependent: :destroy
   enum status: { todo: 0, doing: 1, done: 2 }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project
+  validates :title, presence: true
+  validates :status, presence: true
+  enum status: { todo: 0, doing: 1, done: 2 }
 end
