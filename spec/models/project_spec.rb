2819268abfa7825061984bfc28cb139a24d8779e
yuji91 <yuji91.1227@gmail.com>
2019-08-11 18:41:21 +0900

    Add: バリデーション確認用のModelSpecを追加

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:project) { create(:project) }
+
+  describe 'バリデーション確認' do
+    it '名前が未入力のプロジェクトは作成できないこと' do
+      project = Project.new
+      expect(project.valid?).to be false
+      expect(project.errors[:name]).to include("can't be blank")
+    end
+
+    it '重複した名前のプロジェクトは作成できないこと' do
+      duplicated_project = Project.new(name: project.name)
+      expect(duplicated_project.valid?).to be false
+      expect(duplicated_project.errors[:name]).to include('has already been taken')
+    end
+  end
 end
