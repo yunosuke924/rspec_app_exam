2bddcea41877758c7301d0e8aacf5abdd16ae6d7
yuji91 <yuji91.1227@gmail.com>
2019-08-17 14:44:45 +0900

    Add: ステータスが完了になった際に完了日を登録するコールバックを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,10 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :status, presence: true
   enum status: { todo: 0, doing: 1, done: 2 }
+
+  after_update :insert_complete_date
+
+  def insert_complete_date
+    update(completion_date: Time.current) if done? && completion_date.blank?
+  end
 end
