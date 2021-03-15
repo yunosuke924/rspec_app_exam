02b897897f88cad6ffbaf13e4b148ab5305d9be6
MinegishiYunosuke <y.minegishi0924@gmail.com>
2021-03-15 16:52:48 +0900

    Fix: ApplicationHelperで定義したメソッドをシステムスペックでも使えるように設定

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe 'Task', type: :system do
         fill_in 'Deadline', with: Time.current
         click_button 'Update Task'
         click_link 'Back'
-        expect(find('.task_list')).to have_content(Time.current.strftime("%-m/%d %-H:%M"))
+        expect(find('.task_list')).to have_content(short_time(Time.current))
         expect(current_path).to eq project_tasks_path(project)
       end
 
